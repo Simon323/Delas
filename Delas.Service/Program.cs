0cf273c7c72f5c3280c5bb0300c90fd4dedec348
Simon323 <szmiekus@gmail.com>
2015-12-16 01:37:15 +0100

    Initial commit

---
+++ Delas.Service/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delas.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost serviceHost = new ServiceHost(typeof(Service1)))
+            {
+                serviceHost.Open();
+                Console.WriteLine("Service is open...");
+
+                //
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Delas.Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using Delas.Test.ServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delas.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Service1Client client = new Service1Client("Service1Endpoint"))
+            {
+                while (true)
+                {
+                    if (System.Console.ReadLine().Equals("exit"))
+                        break;
+
+                    System.Console.WriteLine(client.GetBankName());
+
+                }
+            }
+        }
+    }
+}
