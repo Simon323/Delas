7ca92ea62f9d677fc9e413f94fa8055f21d02f79
Simon323 <szmiekus@gmail.com>
2016-01-05 21:43:32 +0100

    Include base REST interface

---
+++ Delas.Rest/Persons.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delas.Rest
+{
+    public partial class Persons
+    {
+        private static readonly Persons _instance = new Persons();
+        private Persons() { }
+        public static Persons Instance
+        {
+            get { return _instance; }
+        }
+        public List<Person> PersonList
+        {
+            get { return persons; }
+        }
+
+        private List<Person> persons = new List<Person>()
+        {
+            new Person() {PersonId = 1, Name = "jan kwalski", Age = 32 }
+        };
+    }
+}
---
+++ Delas.Rest/RESTService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Delas.Rest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RESTService" in both code and config file together.
+    public class RESTService : IRESTService
+    {
+        public List<Person> GetPersonsList()
+        {
+            return Persons.Instance.PersonList;
+        }
+    }
+}
