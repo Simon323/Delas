15b12f4c7d26323a5a7dd1d0204cc2fbfe4efba5
Simon323 <szmiekus@gmail.com>
2016-01-13 13:15:27 +0100

    @SOAP Feature CashHandout

--- Delas.Site/Controllers/HomeController.cs
+++ Delas.Site/Controllers/HomeController.cs
@@ -71,7 +71,37 @@ namespace Delas.Site.Controllers
         [HttpPost]
         public ActionResult CashHandout(PaymentViewModel model) // wypłata
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                var account = client.GetAccountById(model.IdAccount);
+                double balanceAfterOpperation = account.Balance - model.Amount;
+
+                if (model.Amount <= 0 || balanceAfterOpperation < 0)
+                {
+                    ModelState.AddModelError("", "Incorect Amount");
+                    return View(model);
+                }
+
+                AccountSOAP accountToUpdate = new AccountSOAP();
+                accountToUpdate.Id = model.IdAccount;
+                accountToUpdate.Balance = balanceAfterOpperation;
+
+                HistorySOAP history = new HistorySOAP();
+                history.IdAccount = model.IdAccount;
+                history.Title = "Wypłata własna";
+                history.Amount = model.Amount * (-1);
+                history.OperationType = "Wypłata";
+                history.Balance = accountToUpdate.Balance;
+                history.Date = DateTime.Now;
+
+                client.UpdateAccount(accountToUpdate);
+                client.AddHistory(history);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Incorect Amount");
+            return View(model);
         }
 
         public ActionResult Contribution(int id) //wpłata
@@ -91,19 +121,20 @@ namespace Delas.Site.Controllers
                     return View(model);
                 }
 
-                AccountSOAP account = new AccountSOAP();
-                account.Id = model.IdAccount;
-                account.Balance = account.Balance + model.Amount;
+                var account = client.GetAccountById(model.IdAccount);
+                AccountSOAP accountToUpdate = new AccountSOAP();
+                accountToUpdate.Id = model.IdAccount;
+                accountToUpdate.Balance = account.Balance + model.Amount;
 
                 HistorySOAP history = new HistorySOAP();
                 history.IdAccount = model.IdAccount;
                 history.Title = "Wpłata własna";
                 history.Amount = model.Amount;
                 history.OperationType = "Wpłata";
-                history.Balance = account.Balance;
+                history.Balance = accountToUpdate.Balance;
                 history.Date = DateTime.Now;
 
-                client.UpdateAccount(account);
+                client.UpdateAccount(accountToUpdate);
                 client.AddHistory(history);
 
                 return RedirectToAction("Index", "Home");
